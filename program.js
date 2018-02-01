80f43c9ffb3046a71edaf83db7980895caf88d5b
Wiktoria Mikołajów <mikolajow.w@gmail.com>
2018-02-01 13:24:59 +0100

    final project

---
+++ program.js
@@ -0,0 +1,22 @@
+process.stdin.setEncoding('utf-8');
+
+console.log('Available commands: version. language, /exit');
+process.stdin.on('readable', function() {
+   var input = process.stdin.read();
+    if (input !== null) {
+    var instruction = input.toString().trim();
+    switch (instruction){
+        case '/exit':
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+            break;
+        case 'version':
+            console.log(process.version);
+            break;
+        case 'language':
+            console.log(process.env.lang);
+            break;
+        }
+    }
+});
+  
